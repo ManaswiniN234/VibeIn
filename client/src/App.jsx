0fe076f988d94ecfda8255d6c5406a1f847b6aa8
ManaswiniN234 <manaswini.n234@gmail.com>
2025-08-11 12:20:39 +0530

    Installed basic react app and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+          <h1 className="text-4xl font-bold text-blue-600 p-4 rounded-lg shadow-lg bg-white">
+            Hello, Tailwind CSS in React!
+          </h1>
+        </div>
+      
+    </>
+  )
+}
+
+export default App
